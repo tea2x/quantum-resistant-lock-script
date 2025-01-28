fdb0ea78a8a3937429750c930254d48c4836d23f
tea2x <copperheart05@gmail.com>
2025-01-28 16:59:42 +0700

    update script test

--- test_js_c_interop/test.sh
+++ test_js_c_interop/test.sh
@@ -12,12 +12,24 @@ cargo build
 
 # Generate keys using sphinc+ reference implementation
 cd tools/ckb-sphincs-tools || exit
+
+echo -e "\n\n\n######################## SPHINCS+C GEN KEY ########################"
 cargo run -- gen-key ../../test_js_c_interop/key.json
+echo "######################################################################"
 
 # Sign the message using sphinc+ reference implementation
-cargo run -- sign --message_file ../../test_js_c_interop/message.txt --key_file ../../test_js_c_interop/key.json --signature_path ../../test_js_c_interop/signature.txt
+echo -e "\n\n\n######################## SPHINCS+C SIGN ########################"
+cargo run -- sign --message_file ../../test_js_c_interop/message.txt --key_file ../../test_js_c_interop/key.json --signature_file ../../test_js_c_interop/sphincs_signature.txt
+echo "######################################################################"
 
 # Verify using Node.js
 cd ../../test_js_c_interop || exit
 npm install
+# in index.js, noble quantum produces a noble signature with the same key and same message
 node index.js
+
+# Sphincs+ verifying noble sigature
+cd ../tools/ckb-sphincs-tools || exit
+echo -e "\n\n\n######################## SPHINCS+C VERIFY NOBLE SIGNATURE ########################"
+cargo run verify --key_file ../../test_js_c_interop/key.json --message_file ../../test_js_c_interop/message.txt --signature_file ../../test_js_c_interop/noble_signature.txt
+echo "######################################################################"
